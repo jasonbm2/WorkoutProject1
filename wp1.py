f11a72230d1b012e12a5940f0409247ecae93d69
jasonbm2 <jasonbradymartin@gmail.com>
2024-01-26 14:44:51 -0800

    added exceptions

--- wp1.py
+++ wp1.py
@@ -1,41 +1,53 @@
 #Jason Martin
 #90501948
 
-#simple program that streams about 1kb of text at a time from an input file, reverses it, and writes it to an output file.
+#program that streams about 1kb of text at a time from an input file, reverses it, and writes it to an output file.
 
 import argparse
-CHUNK_SIZE = 1024
+CHUNK_SIZE = 1024 #about 1 kilobyte worth of characters
+
 
 def write(reverse_text, output_file):
-    current_chunk = reverse_text[:CHUNK_SIZE]
-    counter = 1
-    with open(output_file, "w") as f_out:
-        while True:
-            f_out.write(current_chunk)
-            current_chunk = reverse_text[CHUNK_SIZE * counter: CHUNK_SIZE * (counter + 1)]
-            counter += 1
-            if not current_chunk:
-                break
+    current_chunk = reverse_text[:CHUNK_SIZE] #grabs one kb worth of characters from the reversed input file
+    counter = 1 #counter to increment through the reversed input file by kbs
+    try:
+        with open(output_file, "w") as f_out:
+            while True:
+                f_out.write(current_chunk)
+                current_chunk = reverse_text[CHUNK_SIZE * counter: CHUNK_SIZE * (counter + 1)] #increments through the reversed input file kb by kb
+                counter += 1
+                if not current_chunk: #exits the loop when the end of the reversed input file has been reached
+                    break
+    except IOError as e:
+        print(f"IO Error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
-
-def reverse_text(input_file):
+def reverse_text(input_file): #streams from the input file and reverses each chunk as it is read
     reverse_chunk = ""
     reverse_text = ""
-    with open(input_file, "r") as f_in:
-        while True:
-            chunk = f_in.read(CHUNK_SIZE)
-            #exit the loop when the end of the file is reached
-            if not len(chunk) > 0:
-                break
+    try:
+        with open(input_file, "r") as f_in:
+            while True:
+                chunk = f_in.read(CHUNK_SIZE)
+                #exit the loop when the end of the file is reached
+                if not len(chunk) > 0:
+                    break
 
-            reverse_chunk = chunk[::-1]
-            reverse_text = reverse_chunk + reverse_text
-    return reverse_text
+                reverse_chunk = chunk[::-1]
+                reverse_text = reverse_chunk + reverse_text #appends the total reversed text to the end of the newly reversed chunk to mimic an actually reversed string
+        return reverse_text
+    except FileNotFoundError as e:
+        print(f"Error: {input_file} not found")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
-def take_input():
-    parser = argparse.ArgumentParser()
+
+
+def take_input(): #receives the input file and output file names as input from the command line
+    parser = argparse.ArgumentParser() #creates parser
 
     parser.add_argument("input_file")
     parser.add_argument("output_file")
@@ -44,6 +56,7 @@ def take_input():
     input_file = args.input_file
     output_file = args.output_file
     return (input_file, output_file)
+
 
 def main():
     input_file, output_file = take_input()
