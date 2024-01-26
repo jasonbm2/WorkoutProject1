2a4618ddb723a0ed38b533b91176ffe7b3f3053f
jasonbm2 <jasonbradymartin@gmail.com>
2024-01-26 01:43:38 -0800

    finished take_input function

--- wp1.py
+++ wp1.py
@@ -1,8 +1,25 @@
 #Jason Martin
 #90501948
+import argparse
+
+def reverse_text(input_file, output_file):
+    with open(input_file, "r") as f:
+        current_line = f.readline()
+
+def take_input():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input_file")
+    parser.add_argument("output_file")
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    output_file = args.output_file
+    return (input_file, output_file)
 
 def main():
-    pass
+    input_file, output_file = take_input()
+    reverse_text(input_file, output_file)
 
 if __name__ == "__main__":
     main()
